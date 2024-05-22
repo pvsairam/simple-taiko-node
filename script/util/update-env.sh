b084fdb81d2b7f149c1dd348e2f570c76eb60e71
Sairam <pvsairam@hotmail.com>
2024-05-22 22:16:16 +0530

    Update update-env.sh
    Changes made:
    
    Temporary files: mktemp is used for creating backup and new files to ensure they are unique and safely handled.
    Quoting and escaping: Proper quoting is ensured to handle special characters and spaces.
    Explicit checks: Added explicit checks for non-empty values using -n.

--- script/util/update-env.sh
+++ script/util/update-env.sh
@@ -12,30 +12,32 @@ set -o pipefail  # Exit on pipe command errors
 update_env() {
     local env_path=$1
     local env_sample_path=$2
+    local env_bak=$(mktemp)
+    local env_new=$(mktemp)
 
-    cp $env_path "${env_path}.bak"  # backup original .env file
+    cp "$env_path" "$env_bak"  # backup original .env file
 
     # Process lines in .env.sample
     while IFS= read -r line || [[ -n "$line" ]]; do
         if [[ $line =~ ^#.*$ || $line == "" ]]; then  # if comment or empty line
-            echo "$line" >> "${env_path}.new"
+            echo "$line" >> "$env_new"
         else
             key=$(echo "$line" | cut -d '=' -f 1)  # extract the key
             value_sample=$(echo "$line" | cut -d '=' -f 2-)  # extract the value from sample
 
             # search for the key in .env file
-            value_env=$(grep "^$key=" "${env_path}.bak" | cut -d '=' -f 2-)
-            if [[ $value_env && -n $value_sample ]]; then
-                echo "$key=$value_sample" >> "${env_path}.new"  # use updated value from .env.sample
-            elif [[ $value_env ]]; then
-                echo "$key=$value_env" >> "${env_path}.new"  # use value from .env if key exists and sample value is empty
+            value_env=$(grep -E "^${key}=" "$env_bak" | cut -d '=' -f 2-)
+            if [[ -n "$value_env" && -n "$value_sample" ]]; then
+                echo "$key=$value_sample" >> "$env_new"  # use updated value from .env.sample
+            elif [[ -n "$value_env" ]]; then
+                echo "$key=$value_env" >> "$env_new"  # use value from .env if key exists and sample value is empty
             else
-                echo "$line" >> "${env_path}.new"  # use default value from .env.sample
+                echo "$line" >> "$env_new"  # use default value from .env.sample
             fi
         fi
     done < "$env_sample_path"
 
-    mv "${env_path}.new" "$env_path"  # replace original .env file with the new one
+    mv "$env_new" "$env_path"  # replace original .env file with the new one
 }
 
 update_env ".env" ".env.sample"
